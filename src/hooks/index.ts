8cb9524f9acd309c6b199f0908bc648c302a9db7
ishida-daiki <ishida-daiki@dmm.com>
2024-08-13 16:31:20 +0900

    hooks の並びを修正

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,6 @@ export * from "./useDiscussionCategories";
 export * from "./useDiscussionLabels";
 export * from "./useElementName";
 export * from "./useFormState";
+export * from "./useHandleClick";
 export * from "./useImageUpload";
 export * from "./useScrollDetection";
-export * from "./useHandleClick";
