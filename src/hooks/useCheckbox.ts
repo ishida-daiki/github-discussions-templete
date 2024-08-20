f07615a7f9dd3f8bc3d99ee36c3f984d0d7a10bd
ishida-daiki <ishida-daiki@dmm.com>
2024-08-20 11:38:42 +0900

    useCheckbox フックにコメントを追加

--- src/hooks/useCheckbox.ts
+++ src/hooks/useCheckbox.ts
@@ -1,6 +1,13 @@
 import { JSX } from "preact";
 import { useState } from "preact/hooks";
 
+/**
+ * useCheckbox フック
+ * 
+ * ActionFooter コンポーネントで使用されるチェックボックスの状態を管理します。
+ * 
+ * @return {[boolean, (event: JSX.TargetedEvent<HTMLInputElement>) => void]} - チェックボックスの状態と変更関数
+ */
 export function useCheckbox(): [
   boolean,
   (event: JSX.TargetedEvent<HTMLInputElement>) => void
